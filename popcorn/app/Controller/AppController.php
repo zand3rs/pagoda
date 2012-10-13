e31fa7a07d0b0c82556cf7853b80e334b6434ec0
zander <zand3rs@gmail.com>
2012-10-13 06:19:42 -0700

    added user login cookie

--- popcorn/app/Controller/AppController.php
+++ popcorn/app/Controller/AppController.php
@@ -15,6 +15,7 @@ class AppController extends Controller {
 
     public $components = array(
         'Session',
+        'Cookie',
         'Auth' => array(
             'loginRedirect' => array('controller' => 'users', 'action' => 'index'),
             'logoutRedirect' => array('controller' => 'users', 'action' => 'login'),
@@ -29,8 +30,22 @@ class AppController extends Controller {
     public function beforeFilter() {
         $this->Auth->authenticate = array('Custom');
         $this->Auth->allow('login', 'logout', 'signup', 'callback');
+        $this->Cookie->name = 'popcorn';
+        $this->Cookie->time = 0;
     }
 
+    //--------------------------------------------------------------------------
+
+    public function afterFilter() {
+        if ($this->Auth->loggedIn()) {
+            if (! $this->Cookie->read('user')) {
+                $this->Cookie->write('user.email', $this->Auth->user('email'), false);
+                $this->Cookie->write('user.access_token', $this->Auth->user('access_token'), false);
+            }
+        } else {
+            $this->Cookie->destroy();
+        }
+    }
 
     //==========================================================================
 
